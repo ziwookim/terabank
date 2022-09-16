87293f504edfbd320f90940fcb0acb78f3b69784
ziwookim <ziwookiim@gmail.com>
2022-09-17 00:22:22 +0900

    project setting

---
+++ src/main/java/terafintech/terabank/Hello.java
@@ -0,0 +1,9 @@
+package terafintech.terabank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String data;
+}
---
+++ src/main/java/terafintech/terabank/TerabankApplication.java
@@ -0,0 +1,18 @@
+package terafintech.terabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerabankApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+		
+		SpringApplication.run(TerabankApplication.class, args);
+	}
+
+}
